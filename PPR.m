9fdd50d0d9f99f065c60406ad31a26b297715164
nonaelly <159625074+nonaelly@users.noreply.github.com>
2024-11-17 21:54:33 +0800

    Create PPR.m

---
+++ PPR.m
@@ -0,0 +1,14 @@
+% PPR cohesive model
+d_t
+d_n
+psi = min(phi_n, phi_t) + 
+
+%% Recognition process
+% step1 Δn <= δn
+if d_n > D_n || (d_n <= D_n && abs(d_t) > D_t)
+% interface failure
+else
+    % normal state judge
+
+    % tangent state judge
+end
